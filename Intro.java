0558918553d6ee24219776f4d3903a81006926ba
Prenaka <karma.prenaka@gmail.com>
2020-05-07 16:51:45 -0400

    first program

---
+++ Intro.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Intro{
+
+public static void main(String[]args){
+
+Scanner input =new Scanner(System.in);
+System.out.println("Enter name:");
+String n=input.next();
+
+System.out.println("Hello\n"+n+"\nWelcome to Java World");
+}
+}
+
